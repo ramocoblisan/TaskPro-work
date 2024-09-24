efa4bb7921f2806a6e166e70ba506731ddec48d1
ramocoblisan <ramonaioana67@yahoo.com>
2024-09-24 23:26:16 +0300

    Mobile LoginPage done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,8 @@
-// src/components/Login/Login.jsx
 import React, { useState } from 'react';
 import styles from './Login.module.css';
+import { useLocation } from 'react-router-dom';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [username, setUsername] = useState('');
@@ -8,35 +10,50 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Aici poți adăuga logica pentru autentificare
     console.log('Username:', username);
     console.log('Password:', password);
   };
 
+  const location = useLocation();
+  
+  const isLoginPage = location.pathname === '/login';
+  const isRegisterPage = location.pathname === '/register';
+
   return (
     <div className={styles.loginContainer}>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className={styles.inputGroup}>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+      <div className={styles.container}>
+        <div className={styles.buttonsContainer}>
+        <Link to="/register" 
+        className={classNames(styles.button, {[styles.activeButton]: isRegisterPage})}>
+          Registration
+        </Link>
+        <Link to="/login" 
+        className={classNames(styles.button, {[styles.activeButton]: isLoginPage})}>
+          Log In
+        </Link>
         </div>
-        <div className={styles.inputGroup}>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" className={styles.submitBtn}>Login</button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.inputGroup}>
+            <input
+              type="text"
+              id="username"
+              placeholder="Enter your email"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <input
+              type="password"
+              id="password"
+              placeholder="Confirm a password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className={styles.submitBtn}>Log In Now</button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/LogInPage/LoginPage.jsx
+++ src/pages/LogInPage/LoginPage.jsx
@@ -4,7 +4,6 @@ import Login from '../../components/Login/Login';
 const LoginPage = () => {
   return (
     <div>
-      <h1>Login Page</h1>
       <Login />
     </div>
   );
