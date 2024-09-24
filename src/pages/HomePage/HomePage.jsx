5549c051b826962d41ec24b5474d94655b4c74a4
ramocoblisan <ramonaioana67@yahoo.com>
2024-09-24 22:38:05 +0300

    Mobile HomePage done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import styles from './HomePage.module.css';
+import logo from '../../images/img+logo.png';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
+
+
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div className={styles.container}>
+      <img src={logo} alt="App Logo" className={styles.logo} />
+      <p className={styles.text}>Supercharge your productivity and take control of your tasks
+         with Task Pro - Don't wait, start achieving your goals now!
+      </p>
+      <div className={styles.authButtons}>
+            <Link to="/register" 
+            className={styles.activeButton}>
+              Registration
+            </Link>
+            <Link to="/login" 
+            className={styles.LoginButton}>
+              Log In
+            </Link>
+        </div>
     </div>
   );
 };
